8e6dc8fa57b0201c241af9c09e4c8db1067fa742
Pranav Prakash <prakash.pran@northeastern.edu>
2025-10-16 17:05:38 -0400

    Two Sum

---
+++ leetcode/1-Two Sum/twosum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap= {}
+        for i,n in enumerate(nums):
+            if target - n in prevMap:
+                return [prevMap[target - n],i]
+            prevMap[n] = i
